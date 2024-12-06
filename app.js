90038feb5b7e80266340e7cc0fcb8f40e9e01754
Mykhailo <kurochkinmy@gmail.com>
2024-12-06 13:41:04 +0200

    add script

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const page = document.documentElement;
+const switcher = document.querySelector('.theme-switcher');
+
+console.log(switcher);
+
+switcher.addEventListener('click', () => {
+  if(page.classList.contains('page--theme--dark')) {
+    page.classList.remove('page--theme--dark')
+    switcher.classList.remove('theme-switcher--theme--dark')
+  } else {
+    page.classList.add('page--theme--dark');
+    switcher.classList.add('theme-switcher--theme--dark');
+  }
+});
