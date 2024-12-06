df8824fa2ab179fa980e9a45ec76e97eb73430d3
Mykhailo <kurochkinmy@gmail.com>
2024-12-06 13:47:48 +0200

    add theme name changing

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const page = document.documentElement;
+const page = document.querySelector('.page');
 const switcher = document.querySelector('.theme-switcher');
-
-console.log(switcher);
+const themeName = document.querySelector('.theme-name');
 
 switcher.addEventListener('click', () => {
   if(page.classList.contains('page--theme--dark')) {
-    page.classList.remove('page--theme--dark')
-    switcher.classList.remove('theme-switcher--theme--dark')
+    page.classList.remove('page--theme--dark');
+    switcher.classList.remove('theme-switcher--theme--dark');
+    themeName.textContent = 'Theme: Light';
   } else {
     page.classList.add('page--theme--dark');
     switcher.classList.add('theme-switcher--theme--dark');
+    themeName.innerHTML = 'Theme: <strong>Dark</strong>';
   }
 });
